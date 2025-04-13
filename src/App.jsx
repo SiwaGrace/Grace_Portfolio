94c15468a39d7c2eb06f6a14c6f259b9f83772a7
missesenam <moneyesenam@gmail.com>
2025-04-13 19:52:02 +0100

    installing shadcn with tailwind v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "./components/ui/button";
 
-const App = () => {
-  return <div className="text-3xl font-bold underline">hsdgt</div>;
-};
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 export default App;
