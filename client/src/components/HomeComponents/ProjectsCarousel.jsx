fe64c81b46cd0759cc8d9e2715d00dbe966fb60b
SiwaGrace <moneyesenam@gmail.com>
2026-01-30 09:01:37 +0000

    added quillpad website

--- client/src/components/HomeComponents/ProjectsCarousel.jsx
+++ client/src/components/HomeComponents/ProjectsCarousel.jsx
@@ -4,12 +4,20 @@ import project from "../../assets/banner.jpg";
 import emezakeduconsulting from "../../assets/Project_Emezak_Educational_Consulting.png";
 import asam from "../../assets/asam.png";
 import Projecteternal from "../../assets/Project_eternal-life.png";
-import ayofitness from "../../assets/ayofitness.png";
 import accountingforesight from "../../assets/accounting-foresight.png";
+import quillpad from "../../assets/quillpad.png";
+import ayofitness from "../../assets/ayofitness.png";
 import wanderlustvoyage from "../../assets/wanderlust-voyage.png";
 
 const ProjectsCarousel = () => {
   const projects = [
+    {
+      title: "Quillpad",
+      description:
+        "A minimal space for deep reflection, vision tracking, and clarity. Designed for those who build.",
+      websiteLink: "https://quillpad.vercel.app/",
+      image: `${quillpad}`,
+    },
     {
       title: "Emezak Educational Consulting",
       description: "An international student consulting platform.",
@@ -29,24 +37,18 @@ const ProjectsCarousel = () => {
       websiteLink: "eternal-life-sigma.vercel.app",
       image: `${Projecteternal}`,
     },
-    // {
-    //   title: "AyoFitness",
-    //   description: "Affordable gym at home.",
-    //   websiteLink: "https://ayo-fitness-gym.vercel.app/",
-    //   image: `${ayofitness}`,
-    // },
-    // {
-    //   title: "Wanderlust Voyage",
-    //   description: "homepage for Wanderlust Voyage.",
-    //   websiteLink: "https://wanderlust-voyage.vercel.app/",
-    //   image: `${wanderlustvoyage}`,
-    // },
     {
       title: "Foresight Consult Ltd",
       description: "accounting and IT solutions business.",
       websiteLink: "https://accounting-foresight-tweaked.vercel.app/",
       image: `${accountingforesight}`,
     },
+    // {
+    //   title: "Wanderlust Voyage",
+    //   description: "homepage for Wanderlust Voyage.",
+    //   websiteLink: "https://wanderlust-voyage.vercel.app/",
+    //   image: `${wanderlustvoyage}`,
+    // },
   ];
 
   const carouselRef = useRef(null);
